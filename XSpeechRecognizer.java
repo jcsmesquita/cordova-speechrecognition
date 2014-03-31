106dbba080af4e847d37fcdbc45041d9a9a02ca2
João Mesquita <jcsmesquita@gmail.com>
2014-03-31 22:03:03 +0100

    dev

--- XSpeechRecognizer.java
+++ XSpeechRecognizer.java
@@ -116,7 +116,7 @@ public class XSpeechRecognizer extends CordovaPlugin {
     //@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
-        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callbackContext.toString()));
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callbackContext.getCallbackId()));
                 
         Handler loopHandler = new Handler(Looper.getMainLooper());
         loopHandler.post(new Runnable() {
