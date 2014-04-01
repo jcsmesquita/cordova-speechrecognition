896a2635f1cc5075c413b5eb362e712ec682b5de
João Mesquita <jcsmesquita@gmail.com>
2014-04-01 15:42:08 +0100

    dev

--- XSpeechRecognizer.java
+++ XSpeechRecognizer.java
@@ -258,6 +258,7 @@ public class XSpeechRecognizer extends CordovaPlugin {
         // Create the intent and set parameters
         final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 5000);
         // intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
 
